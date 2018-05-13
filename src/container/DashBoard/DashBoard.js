1c45919347030bb5422ee824000f9877b622f7bb
黄斌 <591112995@qq.com>
2018-05-13 23:35:11 +0800

    add: popover

--- src/container/DashBoard/DashBoard.js
+++ src/container/DashBoard/DashBoard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {NavBar} from 'antd-mobile'
+import {NavBar, Icon,Popover} from 'antd-mobile'
 import QueueAnim from 'rc-queue-anim';
 import {Switch, Route,Redirect} from 'react-router-dom'
 import NavLinkBar from '../../components/NavLink/NavLink'
@@ -11,6 +11,7 @@ import {getMsgList,sendMsg,recvMsg} from '../../reducer/ChatList-redux'
 import Msg from '../../components/Msg/Msg'
 import Analysis from '../../components/Analysis/Analysis'
 
+const Item = Popover.Item;
 @connect(
 	state=>state,
 	{getMsgList,sendMsg,recvMsg}
@@ -19,6 +20,9 @@ import Analysis from '../../components/Analysis/Analysis'
 class DashBoard extends React.Component{
 	constructor(props){
 		super(props);
+		this.state = {
+			open: true,
+		}
 		if(this.props.location.pathname==='/'){
 			this.props.history.push('/register')
 		}
@@ -74,10 +78,46 @@ class DashBoard extends React.Component{
 		]
 
 		const page = navList.find(item=>item.path === pathname)
+		const myImg = src => <img src={require(`../../components/NavLink/img/${src}.png`)} className="am-icon am-icon-xs" alt="" />;
 		return (
 			<div>
 				{}
-				<NavBar className='fixd-header' mode='dark'>
+				<NavBar className='fixd-header' mode='dark'
+					rightContent={
+						pathname==='/me'?
+						<Popover mask
+						  overlayClassName="fortest"
+						  overlayStyle={{ color: 'currentColor' }}
+						  visible={this.state.visible}
+						  overlay={[
+							(<Item key="4" value="scan" icon={myImg('job')} data-seed="logId">
+							修改个人信息</Item>),
+							(<Item key="5" value="special" icon={myImg('boss')} style={{ whiteSpace: 'nowrap' }}>
+							My Qrcode</Item>),
+							(<Item key="6" value="button ct" icon={myImg('msg')}>
+							  <span style={{ marginRight: 5 }}>Help</span>
+							</Item>),
+						  ]}
+						  align={{
+							overflow: { adjustY: 0, adjustX: 0 },
+							offset: [-10, 0],
+						  }}
+						  onVisibleChange={this.handleVisibleChange}
+						  onSelect={this.onSelect}
+						>
+						  <div style={{
+							height: '100%',
+							padding: '0 15px',
+							marginRight: '-15px',
+							display: 'flex',
+							alignItems: 'center',
+						  }}
+						  >
+							<Icon type="ellipsis" />
+						  </div>
+						</Popover>:null
+					}
+				>
 					{pathname==='/'?null:navList.find(v=>v.path===pathname).title}
 				</NavBar>
 				<div style={{marginTop:45,marginBottom:50}}>
