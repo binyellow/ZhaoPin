897daf59d5c0f41efe64c34f8153a486e5f81075
黄斌 <591112995@qq.com>
2018-05-18 09:42:54 +0800

    edit: boss个人中心修改个人信息添加初始值

--- src/container/BossInfo/BossInfo.js
+++ src/container/BossInfo/BossInfo.js
@@ -1,16 +1,17 @@
 
 import React from 'react'
-import {NavBar,InputItem, TextareaItem, Button,Modal} from 'antd-mobile'
+import {NavBar,InputItem, TextareaItem, Button,Modal,Icon} from 'antd-mobile'
 import AvatarSelector from '../../components/AvatarSelector/AvatarSelector'
 import {connect} from 'react-redux'
 import {Redirect} from 'react-router-dom'
 import {update} from '../../services/user'
 import {LoadData} from '../../reducer/login'
 import {getMsgList} from '../../reducer/ChatList-redux'
+import {getAllUserList} from '../../reducer/UserList-redux'
 
 @connect(
 	state=>state,
-	{LoadData,getMsgList}
+	{LoadData,getMsgList,getAllUserList}
 )
 class BossInfo extends React.Component{
 	constructor(props) {
@@ -19,7 +20,14 @@ class BossInfo extends React.Component{
 			title:'',
 			desc:'',
 			company:'',
-			money:''
+			money:'',
+			avatar:null
+		}
+	}
+	componentWillMount(nextProps,nextState){
+		const {userName} = this.props.login;
+		if(!this.props.UserList.allUserList.some(item=>item.userName===userName)){
+			this.props.getAllUserList()
 		}
 	}
 	componentDidMount(){
@@ -27,7 +35,9 @@ class BossInfo extends React.Component{
             this.props.getMsgList()
         }
 	}
-	handleUpdate = (data) =>{
+	handleUpdate = () =>{
+		const editItem = this.props.UserList.allUserList.find(n=>n.userName===this.props.login.userName);
+		const data = this.getOneProps(this.state,editItem)
 		update(data).then(res=>{
 			if(res.status===200&&res.data.success===true){
 				this.props.LoadData(res.data.data)
@@ -41,27 +51,46 @@ class BossInfo extends React.Component{
 			[key]:val
 		})
 	}
+	getOneProps = (state,editItem) => {
+		const keys = Object.keys(state);
+		const data = {};
+		for (const key of keys) {
+			if(state[key]===''||state[key]===null){
+				data[key] = editItem[key]?editItem[key]:state[key]
+			}else{
+				data[key] = state[key]
+			}
+		}
+		return data
+	}
 	render(){
-		const path = this.props.location.pathname
-		const redirect = this.props.login.redirectTo
+		const path = this.props.location.pathname;
+		const redirect = this.props.login.redirectTo;
+		const {userName} = this.props.login;
+		const editItem = this.props.UserList.allUserList.find(n=>n.userName===userName);
 		return (
 			<div>
 				{redirect&&redirect!==path? <Redirect to={this.props.login.redirectTo}/> :null}
-				<NavBar mode="dark" >BOSS完善信息页</NavBar>
-				<AvatarSelector 
+				<NavBar 
+					mode="dark"
+					icon={<Icon type="left" />}
+					onLeftClick={() => this.props.history.goBack()}
+				>BOSS完善信息页</NavBar>
+				<AvatarSelector
+					selectedAvatar={editItem?editItem.avatar:undefined}
 					selectAvatar={(imgname)=>{
 						this.setState({
 							avatar:imgname
 						})
 					}}
 				></AvatarSelector>
-				<InputItem onChange={(v)=>this.onChange('title',v)}>
+				<InputItem onChange={(v)=>this.onChange('title',v)} clear defaultValue={editItem?editItem.title:null}>
 					招聘职位
 				</InputItem>
-				<InputItem onChange={(v)=>this.onChange('company',v)}>
+				<InputItem onChange={(v)=>this.onChange('company',v)}  clear defaultValue={editItem?editItem.company:null}>
 					公司名称
 				</InputItem>
-				<InputItem onChange={(v)=>this.onChange('money',v)}>
+				<InputItem onChange={(v)=>this.onChange('money',v)}  clear defaultValue={editItem?editItem.money:null}>
 					职位薪资
 				</InputItem>
 				<TextareaItem
@@ -69,11 +98,11 @@ class BossInfo extends React.Component{
 					rows={3}
 					autoHeight
 					title='职位要求'
+					defaultValue={editItem?editItem.desc:null}
 				>
-					
 				</TextareaItem>
 				<Button 
-					onClick={()=>this.handleUpdate(this.state)}
+					onClick={this.handleUpdate}
 					type='primary'>保存</Button>
 			</div>
 			
